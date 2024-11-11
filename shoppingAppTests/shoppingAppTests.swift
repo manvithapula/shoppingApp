3a701a9a752eee9f5cfd89b905c5edd7ba472e26
manvithapula <manvithapula@gmail.com>
2024-11-11 08:45:19 +0530

    Initial Commit

---
+++ shoppingAppTests/shoppingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  shoppingAppTests.swift
+//  shoppingAppTests
+//
+//  Created by admin64 on 11/11/24.
+//
+
+import Testing
+@testable import shoppingApp
+
+struct shoppingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
